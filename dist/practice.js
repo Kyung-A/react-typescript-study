f067c903d4e2c528a5c811972b1dd47086980bf6
EungyeongNam <kyung@likelion.net>
2022-03-21 16:22:11 +0900

    예제 작성 진행중

--- dist/practice.js
+++ dist/practice.js
@@ -1,3 +1,14 @@
 "use strict";
-var message = 'hello word';
-console.log(message);
+var count = 0;
+count += 1;
+count = '갑자기 분위기 문자열';
+var message = 'hello world';
+var done = true;
+var numbers = [1, 2, 3];
+var messages = ['hello', 'world'];
+messages.push(1);
+var mightBeUndefined = undefined;
+var nullableNumber = null;
+var color = 'red'; // 'res', 'orange', 'yellow' 중 하나
+color = 'yellow';
+color = 'green';
--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,34 @@
-const message: string = 'hello word';
-console.log(message);
+function sum(x: number, y: number): number {
+    // 마지막 : number는 해당 함수의 반환값이 숫자라는 것을 명시함
+    return x + y;
+}
+
+sum(1, 5);
+
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+const total = sumArray([1, 2, 3, 4, 5]);
+
+// 아무것도 반환하지 않는 경우는 void를 사용
+function returnNothing(): void {
+    console.log('hello world');
+}
+
+interface Shape {
+    getArea(): number;
+}
+
+class Circle implements Shape {
+    // Shape interfaced의 조건을 충족
+    radius: number;
+
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
