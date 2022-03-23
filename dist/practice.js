51e6dcdd150cdcafbdaa6f414cf3a57cd594331b
EungyeongNam <kyung@likelion.net>
2022-03-23 14:40:40 +0900

    제네릭 사용

--- dist/practice.js
+++ dist/practice.js
@@ -1,12 +1,61 @@
 "use strict";
-var person = {
-    name: '김사랑'
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-var expert = {
-    name: '김개발',
-    skills: ['javascript', 'react'],
+// 제네릭 기본 사용법
+function merge1(a, b) {
+    return __assign(__assign({}, a), b);
+}
+function merge2(a, b) {
+    return __assign(__assign({}, a), b);
+}
+function wrap(param) {
+    return {
+        param: param,
+    };
+}
+var wrapped = wrap(10);
+var merged = merge1({ foo: 1 }, { bar: 1 });
+// string 형식의 배열을 할당
+var items = {
+    list: ['a', 'b', 'c']
 };
-var people = [person, expert];
-var color = 'red';
-var colors = ['red', 'orange'];
-console.log("people:: ".concat(people), "color:: ".concat(color), "colors:: ".concat(colors));
+var lists = {
+    list: [1, 2, 3]
+};
+// 클래스에서 제네릭 사용하기
+var Queue = /** @class */ (function () {
+    function Queue() {
+        this.list = [];
+    }
+    Object.defineProperty(Queue.prototype, "length", {
+        get: function () {
+            return this.list.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Queue.prototype.enqueue = function (item) {
+        this.list.push(item);
+    };
+    Queue.prototype.dequeue = function () {
+        return this.list.shift();
+    };
+    return Queue;
+}());
+var queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
--- src/practice.ts
+++ src/practice.ts
@@ -1,28 +1,66 @@
-// type 사용
-type Person = {
-    name: string;
-    age?: number;
+// 제네릭 기본 사용법
+function merge1(a: any, b: any): any {
+    return {
+        ...a,
+        ...b
+    };
+}
+
+function merge2<A, B>(a: A, b: B): A & B {
+    return {
+        ...a,
+        ...b,
+    };
+}
+
+function wrap<T>(param: T) {
+    return {
+        param,
+    }
+}
+
+const wrapped = wrap(10);
+const merged = merge1({ foo: 1 }, { bar: 1 });
+
+// interface에서 제네릭 사용하기
+interface Items<T> {
+    list: T[]; 
+}
+// string 형식의 배열을 할당
+const items: Items<string> = {
+    list: ['a', 'b', 'c'] 
 };
 
-// & 표시는 interface의 extends같은 역할
-type Developer = Person & {
-    skills: string[];
-};
+// type에 제네릭 사용하기
+type Lists<T> = {
+    list: T[];
+}
+const lists: Lists<number> = {
+    list: [1, 2, 3]
+}
 
-const person: Person = {
-    name: '김사랑'
-};
 
-const expert: Developer = {
-    name: '김개발',
-    skills: ['javascript', 'react'],
-};
+// 클래스에서 제네릭 사용하기
+class Queue<T> {
+    list: T[] = [];
+    get length() {
+        return this.list.length;
+    }
+    enqueue(item: T) {
+        this.list.push(item);
+    }
+    dequeue() {
+        return this.list.shift();
+    }
+}
 
-type People = Person[];
-const people: People = [person, expert];
+const queue = new Queue<number>();
 
-type Color = 'red' | 'orange' | 'yellow';
-const color: Color = 'red';
-const colors: Color[] = ['red', 'orange'];
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
 
-console.log(people, color, colors);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
